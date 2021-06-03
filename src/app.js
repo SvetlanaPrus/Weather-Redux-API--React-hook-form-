4e9c10cf52d27e567ddc23b244721e9b34534fb8
SvetlanaPrus <64580478+SvetlanaPrus@users.noreply.github.com>
2021-06-03 03:59:34 +0200

    Стартовая точка.

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// Components
+import React from 'react';
+
+// Instruments
+
+export const App = () => {
+    return (
+        <main>
+            <h1>Продвинутый виджет погоды</h1>
+        </main>
+    );
+};
+
